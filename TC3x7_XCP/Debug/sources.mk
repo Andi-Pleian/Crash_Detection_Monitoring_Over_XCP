d18803f41ea42603d3431ba85a88cab1f1c3794d
Pleian Andrei Mihai <69640908+Andi-Pleian@users.noreply.github.com>
2023-04-03 16:40:29 +0300

    Initial commit

---
+++ TC3x7_XCP/Debug/sources.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C++_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXX_SRCS := 
+C_SRCS := 
+C_UPPER_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+SRC_SRCS := 
+C++_DEPS := 
+CC_DEPS := 
+COMPILED_SRCS := 
+CPP_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+C_UPPER_DEPS := 
+EXECUTABLES := 
+OBJS := 
+SECONDARY_SIZE := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+Configurations/Debug \
+Configurations \
+Libraries/Infra/Platform/Tricore/Compilers \
+Libraries/Infra/Ssw/TC36A/Tricore \
+Libraries/Service/CpuGeneric/SysSe/Bsp \
+Libraries/iLLD/TC36A/Tricore/Cpu/Irq \
+Libraries/iLLD/TC36A/Tricore/Cpu/Std \
+Libraries/iLLD/TC36A/Tricore/Cpu/Trap \
+Libraries/iLLD/TC36A/Tricore/Gtm/Std \
+Libraries/iLLD/TC36A/Tricore/Mtu/Std \
+Libraries/iLLD/TC36A/Tricore/Pms/Std \
+Libraries/iLLD/TC36A/Tricore/Port/Std \
+Libraries/iLLD/TC36A/Tricore/Scu/Std \
+Libraries/iLLD/TC36A/Tricore/Src/Std \
+Libraries/iLLD/TC36A/Tricore/Stm/Std \
+Libraries/iLLD/TC36A/Tricore/_Impl \
+Libraries/iLLD/TC36A/Tricore/_PinMap \
+
---
+++ TC3x7_XCP/Debug/subdir.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Blinky_LED.c \
+../Cpu0_Main.c \
+../Cpu1_Main.c 
+
+COMPILED_SRCS += \
+./Blinky_LED.src \
+./Cpu0_Main.src \
+./Cpu1_Main.src 
+
+C_DEPS += \
+./Blinky_LED.d \
+./Cpu0_Main.d \
+./Cpu1_Main.d 
+
+OBJS += \
+./Blinky_LED.o \
+./Cpu0_Main.o \
+./Cpu1_Main.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.src: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING C/C++ Compiler'
+	cctc -cs --dep-file="$(basename $@).d" --misrac-version=2004 -D__CPU__=tc36x "-fC:/Users/uif47623/AURIX-v1.9.0-workspace/TC3x7_XCP/Debug/TASKING_C_C___Compiler-Include_paths.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc36x -Y0 -N0 -Z0 -o "$@" "$<" && \
+	if [ -f "$(basename $@).d" ]; then sed.exe -r  -e 's/\b(.+\.o)\b/\/\1/g' -e 's/\\/\//g' -e 's/\/\//\//g' -e 's/"//g' -e 's/([a-zA-Z]:\/)/\L\1/g' -e 's/\d32:/@TARGET_DELIMITER@/g; s/\\\d32/@ESCAPED_SPACE@/g; s/\d32/\\\d32/g; s/@ESCAPED_SPACE@/\\\d32/g; s/@TARGET_DELIMITER@/\d32:/g' "$(basename $@).d" > "$(basename $@).d_sed" && cp "$(basename $@).d_sed" "$(basename $@).d" && rm -f "$(basename $@).d_sed" 2>/dev/null; else echo 'No dependency file to process';fi
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ./%.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean--2e-
+
+clean--2e-:
+	-$(RM) ./Blinky_LED.d ./Blinky_LED.o ./Blinky_LED.src ./Cpu0_Main.d ./Cpu0_Main.o ./Cpu0_Main.src ./Cpu1_Main.d ./Cpu1_Main.o ./Cpu1_Main.src
+
+.PHONY: clean--2e-
+
