ed88e92ae7b382830308d8cedbd5705157fbe218
Pleian Andrei Mihai <69640908+Andi-Pleian@users.noreply.github.com>
2023-05-15 21:28:59 +0300

    autosar updates
    errori in libraries

--- TC3x7_XCP/Debug/EB_Stubs/subdir.mk
+++ TC3x7_XCP/Debug/EB_Stubs/subdir.mk
@@ -4,132 +4,28 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../EB_Stubs/CanIf.c \
 ../EB_Stubs/CanSM.c \
-../EB_Stubs/CanSM_Cbk.c \
-../EB_Stubs/CanSM_GetCurrentComMode.c \
-../EB_Stubs/CanSM_GetVersionInfo.c \
-../EB_Stubs/CanSM_Init.c \
-../EB_Stubs/CanSM_MainFunction.c \
 ../EB_Stubs/CanSM_Merged.c \
-../EB_Stubs/CanSM_RequestComMode.c \
-../EB_Stubs/CanSM_SetBaudrate.c \
-../EB_Stubs/CanSM_StateMachine.c \
-../EB_Stubs/Can_17_McmCan.c \
-../EB_Stubs/ComM.c \
-../EB_Stubs/ComM_ASR32_Rte.c \
-../EB_Stubs/ComM_ASR40_Rte.c \
-../EB_Stubs/ComM_Hsm.c \
-../EB_Stubs/ComM_HsmComMData.c \
-../EB_Stubs/ComM_HsmComMFnct.c \
-../EB_Stubs/ComM_HsmComMPncData.c \
-../EB_Stubs/ComM_HsmComMPncFnct.c \
-../EB_Stubs/ComM_Rte.c \
 ../EB_Stubs/TSMem.c \
-../EB_Stubs/TSPBConfig.c \
-../EB_Stubs/Xcp.c \
-../EB_Stubs/Xcp_CommandProcessor.c \
-../EB_Stubs/Xcp_EventProcessor.c \
-../EB_Stubs/Xcp_EventReceiver.c \
-../EB_Stubs/Xcp_ReceiveProcessor.c \
-../EB_Stubs/Xcp_StimProcessor.c \
-../EB_Stubs/Xcp_TransmitProcessor.c 
+../EB_Stubs/TSPBConfig.c 
 
 COMPILED_SRCS += \
-./EB_Stubs/CanIf.src \
 ./EB_Stubs/CanSM.src \
-./EB_Stubs/CanSM_Cbk.src \
-./EB_Stubs/CanSM_GetCurrentComMode.src \
-./EB_Stubs/CanSM_GetVersionInfo.src \
-./EB_Stubs/CanSM_Init.src \
-./EB_Stubs/CanSM_MainFunction.src \
 ./EB_Stubs/CanSM_Merged.src \
-./EB_Stubs/CanSM_RequestComMode.src \
-./EB_Stubs/CanSM_SetBaudrate.src \
-./EB_Stubs/CanSM_StateMachine.src \
-./EB_Stubs/Can_17_McmCan.src \
-./EB_Stubs/ComM.src \
-./EB_Stubs/ComM_ASR32_Rte.src \
-./EB_Stubs/ComM_ASR40_Rte.src \
-./EB_Stubs/ComM_Hsm.src \
-./EB_Stubs/ComM_HsmComMData.src \
-./EB_Stubs/ComM_HsmComMFnct.src \
-./EB_Stubs/ComM_HsmComMPncData.src \
-./EB_Stubs/ComM_HsmComMPncFnct.src \
-./EB_Stubs/ComM_Rte.src \
 ./EB_Stubs/TSMem.src \
-./EB_Stubs/TSPBConfig.src \
-./EB_Stubs/Xcp.src \
-./EB_Stubs/Xcp_CommandProcessor.src \
-./EB_Stubs/Xcp_EventProcessor.src \
-./EB_Stubs/Xcp_EventReceiver.src \
-./EB_Stubs/Xcp_ReceiveProcessor.src \
-./EB_Stubs/Xcp_StimProcessor.src \
-./EB_Stubs/Xcp_TransmitProcessor.src 
+./EB_Stubs/TSPBConfig.src 
 
 C_DEPS += \
-./EB_Stubs/CanIf.d \
 ./EB_Stubs/CanSM.d \
-./EB_Stubs/CanSM_Cbk.d \
-./EB_Stubs/CanSM_GetCurrentComMode.d \
-./EB_Stubs/CanSM_GetVersionInfo.d \
-./EB_Stubs/CanSM_Init.d \
-./EB_Stubs/CanSM_MainFunction.d \
 ./EB_Stubs/CanSM_Merged.d \
-./EB_Stubs/CanSM_RequestComMode.d \
-./EB_Stubs/CanSM_SetBaudrate.d \
-./EB_Stubs/CanSM_StateMachine.d \
-./EB_Stubs/Can_17_McmCan.d \
-./EB_Stubs/ComM.d \
-./EB_Stubs/ComM_ASR32_Rte.d \
-./EB_Stubs/ComM_ASR40_Rte.d \
-./EB_Stubs/ComM_Hsm.d \
-./EB_Stubs/ComM_HsmComMData.d \
-./EB_Stubs/ComM_HsmComMFnct.d \
-./EB_Stubs/ComM_HsmComMPncData.d \
-./EB_Stubs/ComM_HsmComMPncFnct.d \
-./EB_Stubs/ComM_Rte.d \
 ./EB_Stubs/TSMem.d \
-./EB_Stubs/TSPBConfig.d \
-./EB_Stubs/Xcp.d \
-./EB_Stubs/Xcp_CommandProcessor.d \
-./EB_Stubs/Xcp_EventProcessor.d \
-./EB_Stubs/Xcp_EventReceiver.d \
-./EB_Stubs/Xcp_ReceiveProcessor.d \
-./EB_Stubs/Xcp_StimProcessor.d \
-./EB_Stubs/Xcp_TransmitProcessor.d 
+./EB_Stubs/TSPBConfig.d 
 
 OBJS += \
-./EB_Stubs/CanIf.o \
 ./EB_Stubs/CanSM.o \
-./EB_Stubs/CanSM_Cbk.o \
-./EB_Stubs/CanSM_GetCurrentComMode.o \
-./EB_Stubs/CanSM_GetVersionInfo.o \
-./EB_Stubs/CanSM_Init.o \
-./EB_Stubs/CanSM_MainFunction.o \
 ./EB_Stubs/CanSM_Merged.o \
-./EB_Stubs/CanSM_RequestComMode.o \
-./EB_Stubs/CanSM_SetBaudrate.o \
-./EB_Stubs/CanSM_StateMachine.o \
-./EB_Stubs/Can_17_McmCan.o \
-./EB_Stubs/ComM.o \
-./EB_Stubs/ComM_ASR32_Rte.o \
-./EB_Stubs/ComM_ASR40_Rte.o \
-./EB_Stubs/ComM_Hsm.o \
-./EB_Stubs/ComM_HsmComMData.o \
-./EB_Stubs/ComM_HsmComMFnct.o \
-./EB_Stubs/ComM_HsmComMPncData.o \
-./EB_Stubs/ComM_HsmComMPncFnct.o \
-./EB_Stubs/ComM_Rte.o \
 ./EB_Stubs/TSMem.o \
-./EB_Stubs/TSPBConfig.o \
-./EB_Stubs/Xcp.o \
-./EB_Stubs/Xcp_CommandProcessor.o \
-./EB_Stubs/Xcp_EventProcessor.o \
-./EB_Stubs/Xcp_EventReceiver.o \
-./EB_Stubs/Xcp_ReceiveProcessor.o \
-./EB_Stubs/Xcp_StimProcessor.o \
-./EB_Stubs/Xcp_TransmitProcessor.o 
+./EB_Stubs/TSPBConfig.o 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -152,7 +48,7 @@ EB_Stubs/%.o: ./EB_Stubs/%.src EB_Stubs/subdir.mk
 clean: clean-EB_Stubs
 
 clean-EB_Stubs:
-	-$(RM) ./EB_Stubs/CanIf.d ./EB_Stubs/CanIf.o ./EB_Stubs/CanIf.src ./EB_Stubs/CanSM.d ./EB_Stubs/CanSM.o ./EB_Stubs/CanSM.src ./EB_Stubs/CanSM_Cbk.d ./EB_Stubs/CanSM_Cbk.o ./EB_Stubs/CanSM_Cbk.src ./EB_Stubs/CanSM_GetCurrentComMode.d ./EB_Stubs/CanSM_GetCurrentComMode.o ./EB_Stubs/CanSM_GetCurrentComMode.src ./EB_Stubs/CanSM_GetVersionInfo.d ./EB_Stubs/CanSM_GetVersionInfo.o ./EB_Stubs/CanSM_GetVersionInfo.src ./EB_Stubs/CanSM_Init.d ./EB_Stubs/CanSM_Init.o ./EB_Stubs/CanSM_Init.src ./EB_Stubs/CanSM_MainFunction.d ./EB_Stubs/CanSM_MainFunction.o ./EB_Stubs/CanSM_MainFunction.src ./EB_Stubs/CanSM_Merged.d ./EB_Stubs/CanSM_Merged.o ./EB_Stubs/CanSM_Merged.src ./EB_Stubs/CanSM_RequestComMode.d ./EB_Stubs/CanSM_RequestComMode.o ./EB_Stubs/CanSM_RequestComMode.src ./EB_Stubs/CanSM_SetBaudrate.d ./EB_Stubs/CanSM_SetBaudrate.o ./EB_Stubs/CanSM_SetBaudrate.src ./EB_Stubs/CanSM_StateMachine.d ./EB_Stubs/CanSM_StateMachine.o ./EB_Stubs/CanSM_StateMachine.src ./EB_Stubs/Can_17_McmCan.d ./EB_Stubs/Can_17_McmCan.o ./EB_Stubs/Can_17_McmCan.src ./EB_Stubs/ComM.d ./EB_Stubs/ComM.o ./EB_Stubs/ComM.src ./EB_Stubs/ComM_ASR32_Rte.d ./EB_Stubs/ComM_ASR32_Rte.o ./EB_Stubs/ComM_ASR32_Rte.src ./EB_Stubs/ComM_ASR40_Rte.d ./EB_Stubs/ComM_ASR40_Rte.o ./EB_Stubs/ComM_ASR40_Rte.src ./EB_Stubs/ComM_Hsm.d ./EB_Stubs/ComM_Hsm.o ./EB_Stubs/ComM_Hsm.src ./EB_Stubs/ComM_HsmComMData.d ./EB_Stubs/ComM_HsmComMData.o ./EB_Stubs/ComM_HsmComMData.src ./EB_Stubs/ComM_HsmComMFnct.d ./EB_Stubs/ComM_HsmComMFnct.o ./EB_Stubs/ComM_HsmComMFnct.src ./EB_Stubs/ComM_HsmComMPncData.d ./EB_Stubs/ComM_HsmComMPncData.o ./EB_Stubs/ComM_HsmComMPncData.src ./EB_Stubs/ComM_HsmComMPncFnct.d ./EB_Stubs/ComM_HsmComMPncFnct.o ./EB_Stubs/ComM_HsmComMPncFnct.src ./EB_Stubs/ComM_Rte.d ./EB_Stubs/ComM_Rte.o ./EB_Stubs/ComM_Rte.src ./EB_Stubs/TSMem.d ./EB_Stubs/TSMem.o ./EB_Stubs/TSMem.src ./EB_Stubs/TSPBConfig.d ./EB_Stubs/TSPBConfig.o ./EB_Stubs/TSPBConfig.src ./EB_Stubs/Xcp.d ./EB_Stubs/Xcp.o ./EB_Stubs/Xcp.src ./EB_Stubs/Xcp_CommandProcessor.d ./EB_Stubs/Xcp_CommandProcessor.o ./EB_Stubs/Xcp_CommandProcessor.src ./EB_Stubs/Xcp_EventProcessor.d ./EB_Stubs/Xcp_EventProcessor.o ./EB_Stubs/Xcp_EventProcessor.src ./EB_Stubs/Xcp_EventReceiver.d ./EB_Stubs/Xcp_EventReceiver.o ./EB_Stubs/Xcp_EventReceiver.src ./EB_Stubs/Xcp_ReceiveProcessor.d ./EB_Stubs/Xcp_ReceiveProcessor.o ./EB_Stubs/Xcp_ReceiveProcessor.src ./EB_Stubs/Xcp_StimProcessor.d ./EB_Stubs/Xcp_StimProcessor.o ./EB_Stubs/Xcp_StimProcessor.src ./EB_Stubs/Xcp_TransmitProcessor.d ./EB_Stubs/Xcp_TransmitProcessor.o ./EB_Stubs/Xcp_TransmitProcessor.src
+	-$(RM) ./EB_Stubs/CanSM.d ./EB_Stubs/CanSM.o ./EB_Stubs/CanSM.src ./EB_Stubs/CanSM_Merged.d ./EB_Stubs/CanSM_Merged.o ./EB_Stubs/CanSM_Merged.src ./EB_Stubs/TSMem.d ./EB_Stubs/TSMem.o ./EB_Stubs/TSMem.src ./EB_Stubs/TSPBConfig.d ./EB_Stubs/TSPBConfig.o ./EB_Stubs/TSPBConfig.src
 
 .PHONY: clean-EB_Stubs
 
